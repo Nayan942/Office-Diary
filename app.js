dd6a7203dd3ed3f42e66c5c57099d8af9cb1e1eb
Nayan942 <nayansantoky942@gmail.com>
2020-01-17 01:19:00 +0530

    Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -52,8 +52,8 @@ app.use(function(req,res,next){
 })
 
 // mongoose.connect("mongodb://localhost:27017/node-demo"); 
-
-mongoose.connect("mongodb+srv://Nayan:Nayan@officediary-fms6s.mongodb.net/test?retryWrites=true&w=majority"); 
+mongoose.connect(process.env.databaseURL);
+// mongoose.connect("mongodb+srv://Nayan:Nayan@officediary-fms6s.mongodb.net/test?retryWrites=true&w=majority"); 
 
 
 var taskSchema = new mongoose.Schema({ 
@@ -460,11 +460,11 @@ app.get("/users/:id/:task_id/skip",function(req,res){
 	})
 })
 
-// app.listen(3000, function(){
-// 	console.log("server is running.............................");
-// })
-
-
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
 	console.log("server is running.............................");
 })
+
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+// 	console.log("server is running.............................");
+// })
