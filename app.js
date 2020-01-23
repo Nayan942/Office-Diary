58b90a42b898e9734a8573b4ab0b2514de78c0c9
Nayan942 <nayansantoky942@gmail.com>
2020-01-23 17:34:52 +0530

    cheking for heroku auto-deploy

--- app.js
+++ app.js
@@ -467,7 +467,7 @@ app.get("/users/:id/:task_id/skip",function(req,res){
 
 
 app.get("*",function(req,res){
-	// req.flash("error","Doesn't exists!");
+	req.flash("error","CHEKING FOR ERRORS");
 	res.redirect("/users");
 })
 
